69ea373604e17704b6723167197d5efe20f5fff1
Chukwu Humphrey Onyekachi <106534454+YoungSmooth@users.noreply.github.com>
2023-01-19 23:27:40 +0100

    step-28

--- lib/responsive/feed_card.dart
+++ lib/responsive/feed_card.dart
@@ -12,70 +12,196 @@ class FeedCard extends StatelessWidget {
     return Column(
       mainAxisAlignment: MainAxisAlignment.center,
       children: [
+        // Feed Image header section
+        Padding(
+          padding: const EdgeInsets.symmetric(vertical: 5),
+          child: Row(
+            children: [
+              SizedBox(
+                width: MediaQuery.of(context).size.width * 0.15,
+              ),
+              Container(
+                width: MediaQuery.of(context).size.width * 0.70,
+                decoration: BoxDecoration(
+                  border: Border.all(color: ColorManager.greyS300),
+                  borderRadius: BorderRadius.circular(25),
+                ),
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: [
+                    Row(
+                      children: [
+                        const Padding(
+                          padding: EdgeInsets.only(left: 5),
+                          child: CircleAvatar(
+                            radius: 18,
+                            backgroundColor: mainColor,
+                          ),
+                        ),
+                        Padding(
+                          padding: const EdgeInsets.only(left: 5),
+                          child: Column(
+                            crossAxisAlignment: CrossAxisAlignment.start,
+                            children: [
+                              ConstrainedBox(
+                                constraints: const BoxConstraints(
+                                    minWidth: 0, maxWidth: 120),
+                                child: const Text('Joseph Nwode three',
+                                    style: TextStyle(
+                                        fontWeight: FontWeight.bold,
+                                        overflow: TextOverflow.ellipsis)),
+                              ),
+                              const Text(
+                                'Usher',
+                                style: TextStyle(fontSize: 12),
+                              ),
+                            ],
+                          ),
+                        ),
+                        const Padding(
+                          padding: EdgeInsets.only(left: 5),
+                          child: Icon(
+                            Icons.verified_outlined,
+                            color: Colors.blue,
+                          ),
+                        )
+                      ],
+                    ),
+                    Padding(
+                      padding: const EdgeInsets.only(left: 0),
+                      child: IconButton(
+                          onPressed: () {
+                            showDialog(
+                              context: context,
+                              builder: (context) => Dialog(
+                                child: ListView(
+                                  padding: const EdgeInsets.symmetric(
+                                    vertical: 16,
+                                  ),
+                                  shrinkWrap: true,
+                                  children: [
+                                    'Delete',
+                                  ]
+                                      .map(
+                                        (e) => InkWell(
+                                          onTap: null,
+                                          child: Container(
+                                            padding: const EdgeInsets.symmetric(
+                                                vertical: 12, horizontal: 16),
+                                            child: Text(e),
+                                          ),
+                                        ),
+                                      )
+                                      .toList(),
+                                ),
+                              ),
+                            );
+                          },
+                          icon: const Icon(Icons.more_vert_outlined)),
+                    )
+                  ],
+                ),
+              ),
+              SizedBox(
+                width: MediaQuery.of(context).size.width * 0.15,
+              ),
+            ],
+          ),
+        ),
+
+        // Feed Image section
+        SizedBox(
+          child: Image.network(
+            'https://images.unsplash.com/photo-1672696049977-5ef343a91556?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=687&q=80',
+            width: double.infinity,
+            fit: BoxFit.cover,
+          ),
+        ),
+
+        // Feed Image comment section
         Row(
-          children: [
-            SizedBox(
-              width: MediaQuery.of(context).size.width * 0.15,
-            ),
-            Container(
-              width: MediaQuery.of(context).size.width * 0.70,
-              decoration: BoxDecoration(
-                border: Border.all(color: ColorManager.greyS300),
-                borderRadius: BorderRadius.circular(25),
+          children: const [
+            IconButton(
+              onPressed: null,
+              icon: Icon(
+                Icons.favorite_outline,
               ),
-              child: Row(
-                mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                children: [
-                  Row(
-                    children: [
-                      const Padding(
-                        padding: EdgeInsets.only(left: 5),
-                        child: CircleAvatar(
-                          radius: 20,
-                          backgroundColor: mainColor,
-                        ),
-                      ),
-                      Padding(
-                        padding: const EdgeInsets.only(left: 5),
-                        child: Column(
-                          crossAxisAlignment: CrossAxisAlignment.start,
-                          children: [
-                            ConstrainedBox(
-                              constraints: const BoxConstraints(
-                                  minWidth: 0, maxWidth: 120),
-                              child: const Text('Joseph Nwode three',
-                                  style: TextStyle(
-                                      fontWeight: FontWeight.bold,
-                                      overflow: TextOverflow.ellipsis)),
-                            ),
-                            const Text(
-                              'Usher',
-                              style: TextStyle(fontSize: 12),
-                            ),
-                          ],
-                        ),
-                      ),
-                      const Padding(
-                        padding: EdgeInsets.only(left: 5),
-                        child: Icon(
-                          Icons.verified_outlined,
-                          color: Colors.blue,
-                        ),
-                      )
-                    ],
+            ),
+            IconButton(
+              onPressed: null,
+              icon: Icon(
+                Icons.mode_comment_outlined,
+              ),
+            ),
+            IconButton(
+              onPressed: null,
+              icon: Icon(
+                Icons.speed_outlined,
+              ),
+            ),
+            Expanded(
+              child: Align(
+                alignment: Alignment.bottomRight,
+                child: IconButton(
+                  onPressed: null,
+                  icon: Icon(
+                    Icons.bookmark_border_outlined,
                   ),
-                  const Padding(
-                    padding: EdgeInsets.only(left: 0),
-                    child: IconButton(
-                        onPressed: null, icon: Icon(Icons.more_vert_outlined)),
-                  )
-                ],
+                ),
               ),
-            ),
-            SizedBox(
-              width: MediaQuery.of(context).size.width * 0.15,
             ),
           ],
+        ),
+
+        // Feed Image Description section
+        Container(
+          padding: EdgeInsets.symmetric(horizontal: 16),
+          child: Column(
+            mainAxisSize: MainAxisSize.min,
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              DefaultTextStyle(
+                style: Theme.of(context).textTheme.subtitle2!.copyWith(
+                      fontWeight: FontWeight.w800,
+                    ),
+                child: Text(
+                  '3456 likes',
+                  style: Theme.of(context).textTheme.bodyText2,
+                ),
+              ),
+              Container(
+                width: double.infinity,
+                padding: const EdgeInsets.only(
+                  top: 8,
+                  bottom: 8,
+                ),
+                child: RichText(
+                  text: const TextSpan(
+                    style: TextStyle(color: ColorManager.black),
+                    children: [
+                      TextSpan(
+                        text: 'Username ',
+                        style: TextStyle(fontWeight: FontWeight.bold),
+                      ),
+                      TextSpan(
+                        text: 'This should contain some long description',
+                      ),
+                    ],
+                  ),
+                ),
+              ),
+              InkWell(
+                onTap: null,
+                child: Container(
+                  padding: const EdgeInsets.symmetric(vertical: 4),
+                  child: const Text(
+                    'View all 4567 comments',
+                    style: TextStyle(fontSize: 16, color: ColorManager.grey),
+                  ),
+                ),
+              )
+            ],
+          ),
         ),
       ],
     );
